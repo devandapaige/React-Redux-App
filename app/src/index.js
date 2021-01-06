f02fcb935be5bb7fb8aa21ceb664ecebfb535c68
Amanda Nelson <amanda.gaylord@gmail.com>
2021-01-05 22:32:55 -0500

    createStore with async created in index.js. Provider surrounding the App Component

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+//redux imports needed to create store, provider (with thunk and logger for async) and reducer.
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { stateReducer } from "./reducers/stateReducer";
 
+//create the store with applyMiddleware (with thunk and logger dependencies.)
+const store = createStore(stateReducer, applyMiddleware(thunk, logger));
+//normal rendering with the Provider surrounding the App Component.
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
